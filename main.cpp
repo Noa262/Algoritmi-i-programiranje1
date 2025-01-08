0f6a3f630f4b96d5afde1b74614f0fbdbbf96c31
Noa <nnoa4064@gmail.com>
2025-01-08 17:31:04 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int broj1;
+    int broj2;
+    int rezultat;
+    cout << "Unesi dva broja:" << endl;
+    cin >> broj1;
+    cin >> broj2;
+    rezultat=broj1-broj2;
+    cout << "Razlika tih brojeva je:" << endl;
+    cout << rezultat;
+    return 0;
+}
