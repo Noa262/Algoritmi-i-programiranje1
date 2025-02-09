65647f3181679bc8696ec712effb5df190faa731
Noa <nnoa4064@gmail.com>
2025-02-09 15:48:46 +0100

    Add files via upload

---
+++ Program za računske operacije.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   int broj1;
+   int broj2;
+   int rjesenje;
+    scanf("Prvi broj=%i Drugi broj=%i",&broj1,&broj2);
+   cout<<endl;
+   rjesenje=broj1+broj2;
+   cout<<"Zbroj brojeva je: "<<rjesenje;
+   cout<<endl;
+   rjesenje=broj1-broj2;
+   cout<<"Razlika brojeva je: "<<rjesenje;
+   cout<<endl;
+   rjesenje=broj1*broj2;
+   cout<<"Umnozak brojeva je: "<<rjesenje;
+   cout<<endl;
+   rjesenje=broj1/broj2;
+   cout<<"Kolicnik brojeva je: "<<rjesenje;
+   cout<<endl;
+
+
+
+
+    return 0;
+}
