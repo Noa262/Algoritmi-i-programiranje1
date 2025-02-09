b25170a27eff7977705d43f443cde4fd3a512d0f
Noa <nnoa4064@gmail.com>
2025-02-09 22:27:03 +0100

    Add files via upload

---
+++ Slovo u broj.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char slovo;
+   printf("Molim vas unesite neko slovo: ");
+   cin>>slovo;
+   cout<<endl;
+   printf("Hvala, iznos tog slova u ASCII tablici je: =%i", slovo);
+    return 0;
+}
