9cb235c505e9fb231198a4f81e5062d62806e73e
Noa <nnoa4064@gmail.com>
2025-02-09 16:05:49 +0100

    Add files via upload

---
+++ Pozitivan_negativan broj.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int broj;
+    cout<<"Unesi bilo koji broj: ";
+    cin>>broj;
+    if (broj>0){
+    cout<<"Taj broj je Pozitivan.";
+    }
+    else if (broj<0){
+    cout<<"Broj je Negativan.";
+    }
+    return 0;
+}
