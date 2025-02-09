1193e8fffba6723a9ad0b87096d998f9591e5b94
Noa <nnoa4064@gmail.com>
2025-02-09 16:43:31 +0100

    Add files via upload

---
+++ Broj u slovo_ili_znak.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int broj;
+    printf("Molim Vas, unesite bilo koji pozitivan broj: ");
+    cin>>broj;
+    cout<<endl;
+    printf("Hvala Vam, slovo ili znak koje se nalaze na tom mjestu u ASCII tablici je: =%c",broj);
+    return 0;
+}
