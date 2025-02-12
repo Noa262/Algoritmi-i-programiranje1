c0eb78e758c1aed72c9c512a00db831226cb1732
Noa <nnoa4064@gmail.com>
2025-02-12 10:42:30 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,14 +4,35 @@ using namespace std;
 
 int main()
 {
-    int broj1;
-    int broj2;
-    int rezultat;
-    cout << "Unesi dva broja:" << endl;
-    cin >> broj1;
-    cin >> broj2;
-    rezultat=broj1-broj2;
-    cout << "Razlika tih brojeva je:" << endl;
-    cout << rezultat;
+    int bodovi;
+    cout<<"Unesite broj postignuti bodova: ";
+    cin>>bodovi;
+    int gz5;
+    int gz4;
+    int gz3;
+    int gz2;
+    cout<<"Unesi broj bodova potreban za peticu: ";
+    cin>>gz5;
+    cout<<"Dobro, a sada unesi broj bodova potreban za cetvorku: ";
+    cin>>gz4;
+    cout<<"A sada za trojku: " ;
+    cin>>gz3;
+    cout<<"I na kraju za dvojku: ";
+    cin>>gz2;
+   if (bodovi>=gz5){
+    cout<<"Odlican!";
+   }
+   else if(bodovi>=gz4){
+    cout<<"Vrlo dobar!";
+   }
+   else if(bodovi>=gz3){
+    cout<<"Dobar!";
+   }
+   else if(bodovi>=gz2){
+    cout<<"Dovoljan!";
+   }
+   else cout<<"Nedovoljan";
+
+
     return 0;
 }
