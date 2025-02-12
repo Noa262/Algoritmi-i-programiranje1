cd94237874b998c99938fad3eaacb88c59aeda2d
Noa <nnoa4064@gmail.com>
2025-02-12 10:42:49 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,34 +4,14 @@ using namespace std;
 
 int main()
 {
-    int bodovi;
-    cout<<"Unesite broj postignuti bodova: ";
-    cin>>bodovi;
-    int gz5;
-    int gz4;
-    int gz3;
-    int gz2;
-    cout<<"Unesi broj bodova potreban za peticu: ";
-    cin>>gz5;
-    cout<<"Dobro, a sada unesi broj bodova potreban za cetvorku: ";
-    cin>>gz4;
-    cout<<"A sada za trojku: " ;
-    cin>>gz3;
-    cout<<"I na kraju za dvojku: ";
-    cin>>gz2;
-   if (bodovi>=gz5){
-    cout<<"Odlican!";
-   }
-   else if(bodovi>=gz4){
-    cout<<"Vrlo dobar!";
-   }
-   else if(bodovi>=gz3){
-    cout<<"Dobar!";
-   }
-   else if(bodovi>=gz2){
-    cout<<"Dovoljan!";
-   }
-   else cout<<"Nedovoljan";
+    char a;
+    char b;
+    int abc;
+    printf("Molim, unesi prvo slovo:");
+    cin<<a;
+    printf("Hvala. Molim unesi i drugo slovo:");
+    cin<<b;
+    abc=a do b;
 
 
     return 0;
