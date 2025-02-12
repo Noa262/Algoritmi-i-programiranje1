330b84114865c426a1848251d5272b6b70397eaa
Noa <nnoa4064@gmail.com>
2025-02-12 10:43:10 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,15 +4,52 @@ using namespace std;
 
 int main()
 {
-    char a;
-    char b;
-    int abc;
-    printf("Molim, unesi prvo slovo:");
-    cin<<a;
-    printf("Hvala. Molim unesi i drugo slovo:");
-    cin<<b;
-    abc=a do b;
+  /* //1. zad.
+   int jedinica;
+   int broj;
+   cout<<("Unesite cjeli broj");
+   cin>> broj;
+   jedinica=broj%10;
+   cout<<jedinica;
 
 
-    return 0;
-}
+
+    //2 zad
+
+    int broj1,broj2,iznos;
+    scanf("%i %i",broj1,broj2);
+    iznos=broj1+broj2;
+    printf("Zbroj %i i %i iznosi %i",broj1, broj2, iznos);
+*/
+    //3 zad
+/*
+    int broj;
+    cout<<"Molim Vas, unesite bilo koji pozitivan broj od 1 do 1000: ";
+    cin>>broj;
+    if(broj>1 & broj<=10){
+        cout<<"broj se nalazi u intervalu od 1 do 10. ";}
+        else if(broj>10 & broj<=100){
+            cout<<"broj se nalazi u intervalu od 10 do 100";
+        }
+        else if(broj>100 & broj<=1000){
+            cout<<"broj se nalazi u intervalu od 1 do 1000";
+        }
+        else if (broj>1000){
+            cout<<"Unijeli ste broj koji je Van intervala. ";
+            }
+
+  */     // 4 zad.
+       int broj;
+       cout<<"Unesite bilo koji broj";
+       cin>>broj;
+       if(broj%3==0){
+        cout<<"broj je djeljiv sa 3.";
+       }
+       else if (broj %3 !=0){
+        cout<<"broj nije djeljiv sa 3.";
+       }
+
+        return 0;
+        }
+
+
